e127297f48ff4d2c8f25699233d01509578ee7a9
ryuji <yrejgnl11@163.com>
2023-05-14 20:38:32 +0800

    fix: optimize the code

--- index.js
+++ index.js
@@ -3,27 +3,39 @@ const FULFILLED = 'fulfilled';
 const REJECTED = 'rejected';
 
 class MyPromise {
-  #state = PENDING;
-  #result = undefined
+  #status = PENDING;
+  #result = undefined;
 
   constructor(executor) {
     const resolve = (data) => {
       this.#changeStatus(FULFILLED, data);
     };
+
     const reject = (reason) => {
-      this.#changeStatus(REJECTED, reason);
+      this.#changeStatus(REJECTED, reason)
     };
 
     try {
       executor(resolve, reject);
     } catch (error) {
-      reject(error)
+      reject(error);
     }
   }
 
-  #changeStatus (state, result) {
-    if (this.#state !== PENDING) return;
-    this.#state = state;
+  #changeStatus (status, result) {
+    if (this.#status !== PENDING) {
+      return;
+    }
+    this.#status = status;
     this.#result = result;
   }
 }
+
+const promise = new MyPromise((resolve, reject) => {
+  resolve(123);
+})
+
+console.log(promise)
+
+
+
